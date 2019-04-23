281378cace294e5928f89c241f6cf5f368feaa4a
Nikita <43931371+91nickel@users.noreply.github.com>
2019-04-23 16:24:46 +0300

    Add files via upload

---
+++ Lesson1/errors/js/ui.js
@@ -0,0 +1,10 @@
+'use strict';
+/*
+function handleClick() {
+  openMail();
+}
+const button = window.getElementById('open');
+button.onclick = handleClick();
+*/
+const button = document.getElementById('open');
+button.onclick = () => openMail();
