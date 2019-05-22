52af8713b5beddb4e844caadaaba948f488867fa
91nickel <43931371+91nickel@users.noreply.github.com>
2019-05-23 00:27:29 +0300

    lesson9

--- Lesson9/cart/js/cart.js
+++ Lesson9/cart/js/cart.js
@@ -49,7 +49,7 @@ function getColors() {
             label.setAttribute('for', 'swatch-' + counter + '-' + el.code);
             label.style = 'border-color: ' + el.code + ';';
 
-            console.log(label);
+            //console.log(label);
 
             const span = document.createElement('span');
             span.style = 'background-color: ' + el.code + ';';
@@ -65,7 +65,7 @@ function getColors() {
             }
 
             //console.log(img);
-            
+
             label.appendChild(span);
             label.appendChild(img);
 
@@ -85,8 +85,8 @@ function getSizes() {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', (e) => {
         sizes = JSON.parse(xhr.response);
-        console.log('Размеры');
-        console.log(sizes);
+        //console.log('Размеры');
+        //console.log(sizes);
 
         const container = document.querySelector('#sizeSwatch');
         sizes.forEach((el, i) => {
@@ -103,24 +103,24 @@ function getSizes() {
                 child.classList.add('soldout');
             }
 
-            console.log(child);
+            //console.log(child);
 
             const input = document.createElement('input');
             input.type = 'radio';
             input.name = 'size';
             input.id = 'swatch-' + counter + '-' + el.type;
             input.value = el.type;
-            if (!el.isAvailable){
+            if (!el.isAvailable) {
                 input.setAttribute('disabled', true)
             }
 
-            console.log(input);
+            //console.log(input);
 
             const label = document.createElement('label');
             label.setAttribute('for', 'swatch-' + counter + '-' + el.type);
             label.innerText = el.title;
 
-            console.log(label);
+            //console.log(label);
 
             const img = document.createElement('img');
             if (!el.isAvailable) {
@@ -130,7 +130,7 @@ function getSizes() {
                 img.src = 'https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886';
             }
 
-            console.log(img);
+            //console.log(img);
 
             label.appendChild(img);
 
@@ -145,6 +145,102 @@ function getSizes() {
     xhr.send();
 
 }
+
+function getCart() {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', (e) => {
+        cart = JSON.parse(xhr.response);
+        console.log('Корзина');
+        console.log(cart);
+
+        const container = document.querySelector('#quick-cart');
+        cart.forEach((el, i) => {
+            const counter = i + 1;
+
+            //<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-2721888517" style="opacity: 1;">
+            const child = document.createElement('div');
+            child.classList.add('quick-cart-product');
+            child.classList.add('quick-cart-product-static');
+            child.id = 'quick-cart-product-' + el.productId;
+            child.style = 'opacity: 1;'
+
+            console.log(child);
+
+            //<div class="quick-cart-product-wrap"></div>
+            const div = document.createElement('div');
+            div.classList.add('quick-cart-product-wrap');
+
+            console.log(div);
+
+            //<img src="https://neto-api.herokuapp.com/hj/3.3/cart/product_1024x1024.png" title="Tony Hunfinger T-Shirt New York">
+            const img = document.createElement('img');
+            img.src = el.pic;
+            img.title = el.title;
+
+            console.log(img);
+
+            //<span class="s1" style="background-color: #000; opacity: .5">$800.00</span>
+            const innerSpan1 = document.createElement('span');
+            innerSpan1.classList.add('s1');
+            innerSpan1.style = 'background-color: #000; opacity: .5';
+            innerSpan1.innerText = '$' + el.price + '.00';
+
+            console.log(innerSpan1);
+
+            //<span class="s2"></span>
+            const innerSpan2 = document.createElement('span');
+            innerSpan2.classList.add('s2');
+
+            console.log(innerSpan2);
+
+            //<span class="count hide fadeUp" id="quick-cart-product-count-2721888517">1</span>
+            const span1 = document.createElement('span');
+            span1.classList.add('count');
+            span1.classList.add('hide');
+            span1.classList.add('fadeUp');
+            span1.id = 'quick-cart-product-count-' + el.productId;
+            span1.innerText = el.quantity;
+
+            console.log(span1);
+
+            //<span class="quick-cart-product-remove remove" data-id="2721888517"></span>
+            const span2 = document.createElement('span');
+            span2.classList.add('couquick-cart-product-remove');
+            span2.classList.add('remove');
+            span2.dataset.id = el.productId;
+
+            console.log(span2);
+
+            div.appendChild(img);
+            div.appendChild(innerSpan1);
+            div.appendChild(innerSpan2);
+
+            child.appendChild(div);
+            child.appendChild(span1);
+            child.appendChild(span2);
+
+            container.appendChild(child);
+        })
+    });
+
+    xhr.open('GET', 'https://neto-api.herokuapp.com/cart');
+    xhr.send();
+
+
+}
+// Корзина
+/*
+<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-2721888517" style="opacity: 1;">
+  <div class="quick-cart-product-wrap">
+    <img src="https://neto-api.herokuapp.com/hj/3.3/cart/product_1024x1024.png" title="Tony Hunfinger T-Shirt New York">
+    <span class="s1" style="background-color: #000; opacity: .5">$800.00</span>
+    <span class="s2"></span>
+  </div>
+  <span class="count hide fadeUp" id="quick-cart-product-count-2721888517">1</span>
+  <span class="quick-cart-product-remove remove" data-id="2721888517"></span>
+</div>
+*/
+
 // Размеры
 
 /*
@@ -168,16 +264,3 @@ function getSizes() {
   </label>
 </div>
 */
-
-
-function getCart() {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', (e) => {
-        cart = JSON.parse(xhr.response);
-        console.log('Корзина');
-        console.log(cart);
-    });
-
-    xhr.open('GET', 'https://neto-api.herokuapp.com/cart');
-    xhr.send();
-}
